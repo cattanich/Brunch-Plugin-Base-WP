6acd33a1365c3d7f009e335c0532ecafbf27326e
Christian <web@kleintours.com.ec>
2019-11-15 14:35:26 -0500

    updates

--- public/scripts/goquoting.js
+++ public/scripts/goquoting.js
@@ -1,3 +1,306 @@
+(function() {
+  'use strict';
+
+  var globals = typeof global === 'undefined' ? self : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = {}.hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    return aliases[name] ? expandAlias(aliases[name]) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (bundle && typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = typeof window === 'undefined' ? this : window;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("goquoting-admin.js", function(exports, require, module) {
+$('#sync-receptivo').click( function(){
+    var quoteId = $('#quoteID').val();
+    $.ajax({
+        type: 'POST',
+        url: ajaxurl,
+        data : {
+            action: 'goDownloadReceptivo',
+            quoteId: quoteId,
+        },
+        beforeSend  : function(){
+            $('#syncmsg').modal('show');
+            $('#sync-text-msg')
+                .removeClass('bg-danger')
+                .removeClass('bg-warning')
+                .removeClass('bg-success')
+                .addClass('bg-info');
+            $('#sync-text-msg .dashicons')
+                .removeClass('dashicons-yes')
+                .removeClass('dashicons-no')
+                .removeClass('dashicons-warning')
+                .addClass('dashicons-image-rotate');
+            $('#sync-text-msg p')
+                .text('Connecting to ELIGOS...');
+        },
+        success: function( response ){
+            setTimeout( function(){
+                if (response.length > 0){
+                    $('#sync-text-msg')
+                        .removeClass('bg-info')
+                        .addClass('bg-success');
+                    $('#sync-text-msg .dashicons')
+                        .removeClass('dashicons-image-rotate')
+                        .addClass('dashicons-yes');
+                    $('#sync-text-msg p')
+                        .text('The information has been downloaded to ELIGOS...');
+                    $('.modal-footer')
+                        .removeClass('hidden');
+                    if(response == 'ERROR'){
+                        $('#q-code').html('<span class="text-danger"><strong>ERROR</strong></span>');
+                    }else{
+                        //$('#q-code').text(response);
+                        console.log(response);
+                    }
+                }else{
+                    $('#sync-text-msg')
+                        .removeClass('bg-info')
+                        .addClass('bg-warning');                
+                    $('#sync-text-msg .dashicons')
+                        .removeClass('dashicons-image-rotate')
+                        .addClass('dashicons-warning');
+                    $('#sync-text-msg p')
+                        .text('An error has occurred with ELIGOS, please try again!...');
+                    $('.modal-footer')
+                        .removeClass('hidden');
+                }
+            }, 1000);    
+            console.log(response);
+        },
+        error: function( response ){
+            setTimeout( function(){
+                $('#sync-text-msg')
+                    .removeClass('bg-info')
+                    .addClass('bg-danger');                
+                $('#sync-text-msg .dashicons')
+                    .removeClass('dashicons-image-rotate')
+                    .addClass('dashicons-no');
+                $('#sync-text-msg p')
+                    .html('Error Connecting to ELIGOS... <strong class="text-danger">please report!</strong>');
+                $('.modal-footer').show();
+                $('.modal-footer')
+                    .removeClass('hidden');
+            }, 1000);
+        }
+    })
+})
+$('#send-quote').click(function(){
+    $('#send-quote-to').modal('show');
+})
+function printDiv() 
+{
+    var divToPrint=document.getElementById('print-area');
+    var newWin=window.open('','Print-Window');
+    var vistaPrint = '';
+    vistaPrint += '<html>';
+    vistaPrint += '<head>';
+    vistaPrint += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+    vistaPrint += '</head>';
+    vistaPrint += '<body onload="window.print()">';
+    vistaPrint += divToPrint.innerHTML;
+    vistaPrint += '</body>';
+    vistaPrint += '</html>';
+
+    console.log(vistaPrint);
+
+    newWin.document.open();
+    newWin.document.write(vistaPrint);
+    //newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+    newWin.document.close();
+    setTimeout(function(){newWin.close();},10);
+}
+function enviarQuote(){
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var campoemail = $('[name=emailto]');
+    var mailfrom = $('[name=emailfrom]');
+    var messagge = $('[name=topmsg]');
+    var body = $('#print-area');
+    
+    console.log(mailfrom.val());
+    console.log(campoemail.val());
+    
+    if(!regex.test(campoemail.val())){
+        $('.email-to').addClass('has-error');
+        campoemail.focus();
+    }else{
+        
+        $('.email-to')
+            .removeClass('has-error')
+            .addClass('has-success');
+        
+        //enviar_quote_via_ajax
+        $.ajax({
+            type: 'POST',
+            url: ajaxurl,
+            data : {
+                action: 'enviar_quote_via_ajax',
+                mailfrom: mailfrom.val(),
+                mailto: campoemail.val(),
+                messagge: messagge.val(),
+                body: body.html(),
+            },
+            beforeSend: function(){
+                $('#send-quote-now')
+                    .removeClass('btn-primary')
+                    .addClass('btn-warning');
+            },
+            success: function( response ){
+                $('#send-quote-now')
+                    .removeClass('btn-warning')
+                    .addClass('btn-success');
+                $('#send-quote-to').modal('hide');
+                
+            },
+            error: function(){
+                $('#send-quote-now')
+                    .removeClass('btn-warning')
+                    .addClass('btn-danger');
+            }
+        });
+    }
+}
+});
+
+;require.register("initialize.js", function(exports, require, module) {
 // const $ = require('jquery');
 // window.jQuery = $;
 // window.$ = $;
@@ -541,3 +844,11 @@ $('#send-as-quote').click(function(e){
 
 
 });
+});
+
+require.register("___globals___", function(exports, require, module) {
+  
+});})();require('___globals___');
+
+
+//# sourceMappingURL=goquoting.js.map
